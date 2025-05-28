90e2ed573bd8f2d00ccc4b98177fc9405cb1f8bf
yuuki-1337 <kaueyuukiyamamoto@gmail.com>
2025-05-28 12:43:31 -0300

    Finalizacao do html

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function rolarParaBaixo(){
+    const secao = document.getElementById('apresentacao');
+    secao.scrollIntoView({behavior: "smooth"});
+}
+function rolarParaHistoria(){
+    const secao = document.getElementById('historia');
+    secao.scrollIntoView({behavior: "smooth"});
+}
+function rolarParaImpactos(){
+    const secao = document.getElementById('impactos');
+    secao.scrollIntoView({behavior: "smooth"});
+}
+function rolarParaVicios(){
+    const secao = document.getElementById('vicios');
+    secao.scrollIntoView({behavior: "smooth"});
+}
+function rolarParaSeguranca(){
+    const secao = document.getElementById('seguranca');
+    secao.scrollIntoView({behavior: "smooth"});
+}
+function rolarParaFuturo(){
+    const secao = document.getElementById('futuro');
+    secao.scrollIntoView({behavior: "smooth"});
+}
